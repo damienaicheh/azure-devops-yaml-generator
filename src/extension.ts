93e5beafd82b188c73cbec715ce514f43270a748
Damien AICHEH <damien.aicheh@gmail.com>
2020-04-11 10:22:34 +0200

    Add name to yaml file.

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,3 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
@@ -10,18 +8,22 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "azuredevopsyamlgenerator" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('azuredevopsyamlgenerator.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('azuredevopsyamlgenerator.helloWorld', async () => {
+		const result = await vscode.window.showInputBox({
+			value: 'azure-pipelines.yml',
+			placeHolder: 'Give a name to your yaml pipeline file',
+			validateInput: text => {
+				return text.endsWith('.yml') ? null : 'Make sure your file extension is \'.yml\'';
+			}
+		});
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from AzureDevOpsYamlGenerator!');
+		vscode.window.showInformationMessage(`You choose: ${result}`);
+
+		//		vscode.window.showInformationMessage('Reload Hello World from AzureDevOpsYamlGenerator!');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
