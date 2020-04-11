3bac1fa41d393e3601054754d7863de86e962c4e
Damien AICHEH <damien.aicheh@gmail.com>
2020-04-11 11:39:23 +0200

    Default two steps.

--- src/extension.ts
+++ src/extension.ts
@@ -1,15 +1,12 @@
-import * as vscode from 'vscode';
+import { window, commands, ExtensionContext, QuickPickItem, Disposable, CancellationToken, QuickInputButton, QuickInput, QuickInputButtons, Uri } from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export function activate(context: ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "azuredevopsyamlgenerator" is now active!');
+	let disposable = commands.registerCommand('azuredevopsyamlgenerator.helloWorld', async () => {
 
-	let disposable = vscode.commands.registerCommand('azuredevopsyamlgenerator.helloWorld', async () => {
-		const result = await vscode.window.showInputBox({
+		const result = await window.showInputBox({
 			value: 'azure-pipelines.yml',
 			placeHolder: 'Give a name to your yaml pipeline file',
 			validateInput: text => {
@@ -17,9 +14,10 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 		});
 
-		vscode.window.showInformationMessage(`You choose: ${result}`);
+		window.showInformationMessage(`You choose: ${result}`);
 
-		//		vscode.window.showInformationMessage('Reload Hello World from AzureDevOpsYamlGenerator!');
+		const state = {} as Partial<State>;
+		await MultiStepInput.run(input => configureYaml(context, input, state));
 	});
 
 	context.subscriptions.push(disposable);
@@ -27,3 +25,239 @@ export function activate(context: vscode.ExtensionContext) {
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
+
+interface State {
+	title: string;
+	step: number;
+	totalSteps: number;
+	resourceGroup: QuickPickItem | string;
+	name: string;
+	runtime: QuickPickItem;
+}
+
+export async function configureYaml(context: ExtensionContext, input: MultiStepInput, state: Partial<State>) {
+
+	class MyButton implements QuickInputButton {
+		constructor(public iconPath: { light: Uri; dark: Uri; }, public tooltip: string) { }
+	}
+
+	const createResourceGroupButton = new MyButton({
+		dark: Uri.file(context.asAbsolutePath('resources/dark/add.svg')),
+		light: Uri.file(context.asAbsolutePath('resources/light/add.svg')),
+	}, 'Create Resource Group');
+
+	const resourceGroups: QuickPickItem[] = ['Xamarin.iOS', 'Xamarin.Android', 'Xamarin.Forms', 'UWP', 'iOS', 'Android']
+		.sort((a, b) => (a > b ? -1 : 1)).map(label => ({ label }));
+
+
+	const title = 'Which technology do you use?';
+
+	const pick = await input.showQuickPick({
+		title,
+		step: 1,
+		totalSteps: 3,
+		placeholder: '',
+		items: resourceGroups,
+		activeItem: typeof state.resourceGroup !== 'string' ? state.resourceGroup : undefined,
+		buttons: [createResourceGroupButton],
+		shouldResume: shouldResume
+	});
+	if (pick instanceof MyButton) {
+		window.showInformationMessage("Button");
+		//return (input: MultiStepInput) => inputResourceGroupName(input, state);
+	} else {
+		state.resourceGroup = pick;
+		window.showInformationMessage("Pick");
+	}
+
+	function shouldResume() {
+		// Could show a notification with the option to resume.
+		return new Promise<boolean>((resolve, reject) => {
+
+		});
+	}
+
+	window.showInformationMessage('Generation done.');
+}
+
+// -------------------------------------------------------
+// Helper code that wraps the API for the multi-step case.
+// -------------------------------------------------------
+
+class InputFlowAction {
+	private constructor() { }
+	static back = new InputFlowAction();
+	static cancel = new InputFlowAction();
+	static resume = new InputFlowAction();
+}
+
+type InputStep = (input: MultiStepInput) => Thenable<InputStep | void>;
+
+interface QuickPickParameters<T extends QuickPickItem> {
+	title: string;
+	step: number;
+	totalSteps: number;
+	items: T[];
+	activeItem?: T;
+	placeholder: string;
+	buttons?: QuickInputButton[];
+	shouldResume: () => Thenable<boolean>;
+}
+
+interface InputBoxParameters {
+	title: string;
+	step: number;
+	totalSteps: number;
+	value: string;
+	prompt: string;
+	validate: (value: string) => Promise<string | undefined>;
+	buttons?: QuickInputButton[];
+	shouldResume: () => Thenable<boolean>;
+}
+
+class MultiStepInput {
+
+	static async run<T>(start: InputStep) {
+		const input = new MultiStepInput();
+		return input.stepThrough(start);
+	}
+
+	private current?: QuickInput;
+	private steps: InputStep[] = [];
+
+	private async stepThrough<T>(start: InputStep) {
+		let step: InputStep | void = start;
+		while (step) {
+			this.steps.push(step);
+			if (this.current) {
+				this.current.enabled = false;
+				this.current.busy = true;
+			}
+			try {
+				step = await step(this);
+			} catch (err) {
+				if (err === InputFlowAction.back) {
+					this.steps.pop();
+					step = this.steps.pop();
+				} else if (err === InputFlowAction.resume) {
+					step = this.steps.pop();
+				} else if (err === InputFlowAction.cancel) {
+					step = undefined;
+				} else {
+					throw err;
+				}
+			}
+		}
+		if (this.current) {
+			this.current.dispose();
+		}
+	}
+
+	async showQuickPick<T extends QuickPickItem, P extends QuickPickParameters<T>>({ title, step, totalSteps, items, activeItem, placeholder, buttons, shouldResume }: P) {
+		const disposables: Disposable[] = [];
+		try {
+			return await new Promise<T | (P extends { buttons: (infer I)[] } ? I : never)>((resolve, reject) => {
+				const input = window.createQuickPick<T>();
+				input.title = title;
+				input.step = step;
+				input.totalSteps = totalSteps;
+				input.placeholder = placeholder;
+				input.items = items;
+
+				if (activeItem) {
+					input.activeItems = [activeItem];
+				}
+
+				input.buttons = [
+					...(this.steps.length > 1 ? [QuickInputButtons.Back] : []),
+					...(buttons || [])
+				];
+
+				disposables.push(
+					input.onDidTriggerButton(item => {
+						if (item === QuickInputButtons.Back) {
+							reject(InputFlowAction.back);
+						} else {
+							resolve(<any>item);
+						}
+					}),
+					input.onDidChangeSelection(items => resolve(items[0])),
+					input.onDidHide(() => {
+						(async () => {
+							reject(shouldResume && await shouldResume() ? InputFlowAction.resume : InputFlowAction.cancel);
+						})()
+							.catch(reject);
+					})
+				);
+
+				if (this.current) {
+					this.current.dispose();
+				}
+
+				this.current = input;
+				this.current.show();
+			});
+		} finally {
+			disposables.forEach(d => d.dispose());
+		}
+	}
+
+	async showInputBox<P extends InputBoxParameters>({ title, step, totalSteps, value, prompt, validate, buttons, shouldResume }: P) {
+		const disposables: Disposable[] = [];
+		try {
+			return await new Promise<string | (P extends { buttons: (infer I)[] } ? I : never)>((resolve, reject) => {
+				const input = window.createInputBox();
+				input.title = title;
+				input.step = step;
+				input.totalSteps = totalSteps;
+				input.value = value || '';
+				input.prompt = prompt;
+				input.buttons = [
+					...(this.steps.length > 1 ? [QuickInputButtons.Back] : []),
+					...(buttons || [])
+				];
+				let validating = validate('');
+				disposables.push(
+					input.onDidTriggerButton(item => {
+						if (item === QuickInputButtons.Back) {
+							reject(InputFlowAction.back);
+						} else {
+							resolve(<any>item);
+						}
+					}),
+					input.onDidAccept(async () => {
+						const value = input.value;
+						input.enabled = false;
+						input.busy = true;
+						if (!(await validate(value))) {
+							resolve(value);
+						}
+						input.enabled = true;
+						input.busy = false;
+					}),
+					input.onDidChangeValue(async text => {
+						const current = validate(text);
+						validating = current;
+						const validationMessage = await current;
+						if (current === validating) {
+							input.validationMessage = validationMessage;
+						}
+					}),
+					input.onDidHide(() => {
+						(async () => {
+							reject(shouldResume && await shouldResume() ? InputFlowAction.resume : InputFlowAction.cancel);
+						})()
+							.catch(reject);
+					})
+				);
+				if (this.current) {
+					this.current.dispose();
+				}
+				this.current = input;
+				this.current.show();
+			});
+		} finally {
+			disposables.forEach(d => d.dispose());
+		}
+	}
+}
